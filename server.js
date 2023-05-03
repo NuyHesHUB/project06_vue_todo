95cb5162da848061f2829c82b08279c5b4a7900b
joosehyeon <7upbeat7@naver.com>
2023-05-04 07:35:33 +0900

    edit

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
-
-const app = express()
-const port = process.env.PORT || 3000
-
-// SERVES FILES from our dist directory which now contains out index.html
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
-app.listen(port)
-console.log('Listening on port:' + port)
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.listen(port, () => console.log(`Example app listening on port port!`))
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
